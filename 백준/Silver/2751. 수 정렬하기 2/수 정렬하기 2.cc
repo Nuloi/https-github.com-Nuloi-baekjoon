7205431a253af8889d86b75e7952f8c049c65688
Teriri <108971280+Nuloi@users.noreply.github.com>
2022-09-01 21:42:14 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 456 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS    
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1<num2)
+	{
+		return -1;
+	}
+	if (num1 > num2)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int main() {
+	int z;
+	scanf("%d", &z);
+	int dota[z];
+	for (int i = 0; i < z; i++)
+	{
+		scanf("%d", &dota[i]);
+	}
+	qsort(dota, sizeof(dota) / sizeof(int), sizeof(int), compare);
+	for (int i = 0; i < z; i++)
+	{
+		printf("%d\n", dota[i]);
+	}
+}
