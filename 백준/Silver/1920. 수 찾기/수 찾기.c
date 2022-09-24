f8622d5e932ee277672a7c7325a45a5a9a9db1dd
Teriri <108971280+Nuloi@users.noreply.github.com>
2022-09-24 23:13:42 +0900

    [Silver IV] Title: 수 찾기, Time: 80 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.c
+++ 백준/Silver/1920. 수 찾기/수 찾기.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare(const void* a, const void* b) {int num1 = *(int*)a;
+int compare(const void* a, const void* b) {
+	int num1 = *(int*)a;
 	int num2 = *(int*)b;
 
 	if (num1 < num2)
@@ -17,29 +18,31 @@ int compare(const void* a, const void* b) {int num1 = *(int*)a;
 }
 
 
-int bray(int data[], int a, int size) {
-	int low, high, mid;
-	low = 0;
-	high = size - 1;
-	while (1) {
-		mid = (low + high) / 2;
-		if (data[mid] == a) return 1;
-		if (data[low] == a) return 1;
-		if (data[high] == a) return 1;
-
-		if (data[mid] < a)
-			low = mid + 1;
-		else
-			high = mid - 1;
-		if (low >= high)
-			return 0;
-	}
+int bray(int data[], int a, int high) {//틀린거
+    int low = 0;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (data[mid] == a)
+        {
+            return 1;
+        }
+        else if (data[mid] > a)
+        {
+            high = mid - 1;
+        }
+        else if (data[mid] < a)
+        {
+            low = mid + 1;
+        }
+    }
+    return 0;
 }
 
 int main(void) {
 	int n;
 	scanf("%d", &n);
-	int* arr = (int*)malloc(n * sizeof(int));
+	int arr[n];
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
@@ -47,7 +50,7 @@ int main(void) {
 
 	int m;
 	scanf("%d", &m);
-	int* arr2 = (int*)malloc(m * sizeof(int));
+	int arr2[m];
 
 	for (int i = 0; i < m; i++) {
 		scanf("%d", &arr2[i]);
@@ -56,7 +59,7 @@ int main(void) {
 	qsort(arr, n, sizeof(int), compare);
 
 	for (int j = 0; j < m; j++) {
-		printf("%d\n", bray(arr, arr2[j], n));
+		printf("%d\n", bray(arr, arr2[j], n-1));
 	}
 	return 0;
 }
