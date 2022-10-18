da4b66e52bfe0816d44452661499cdf4ed20c3a5
Teriri <108971280+Nuloi@users.noreply.github.com>
2022-10-18 21:32:49 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS    
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d", a - 1946);
+}
