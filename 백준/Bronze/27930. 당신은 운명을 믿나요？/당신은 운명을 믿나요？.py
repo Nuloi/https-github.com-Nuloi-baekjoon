3c7ec9132c91e7e6b4cfeadd007282f5ce648344
Teriri <108971280+Nuloi@users.noreply.github.com>
2023-04-04 01:56:54 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 44 ms, Memory: 32236 KB -BaekjoonHub

---
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
@@ -0,0 +1,15 @@
+sc=input()
+if "K" in sc:
+    if "O" in sc[sc.index("K"):]:
+        if "R" in sc[sc.index("O"):]:
+            if "E" in sc[sc.index("R"):]:
+                if "A" in sc[sc.index("E"):]:
+                    print("KOREA")
+else:
+    if "Y" in sc:
+        if "O" in sc[sc.index("Y"):]:
+            if "N" in sc[sc.index("O"):]:
+                if "S" in sc[sc.index("N"):]:
+                    if "E" in sc[sc.index("S"):]:
+                        if "I" in sc[sc.index("E"):]:
+                            print("YONSEI")
