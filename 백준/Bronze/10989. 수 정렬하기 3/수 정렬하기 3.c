88556d89fa7ab7b0bea113365701458e1b833cef
Teriri <108971280+Nuloi@users.noreply.github.com>
2023-05-02 20:58:30 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2632 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.c
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.c
@@ -1,21 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS    
-#include <stdio.h>
-#include <string.h>
-int main() {
-	long long a;
-	scanf("%lld", &a);
-	int arr[10001] = { 0 };
-	while (a != 0)
-	{
-		int b;
-		scanf("%d", &b);
-		arr[b] += 1;
-		a--;
-	}
-	for(int i = 1 ; i <= 10000; i++)
-	{
-		for (int j = 0; j < arr[i]; j++) {
-			printf("%d\n", i);
+	#define _CRT_SECURE_NO_WARNINGS    
+	#include <stdio.h>
+	#include <string.h>
+	#include <stdlib.h>
+	#include <math.h>
+
+	int main(void) {
+		int arr[10001] = { 0 };
+		int n;
+		scanf("%d", &n);
+		while (n != 0){
+			int b;
+			scanf("%d", &b);
+			arr[b] += 1;
+			n--;
+		}
+		for(int i = 1 ; i <= 10000; i++)
+		{
+			for (int j = 0; j < arr[i]; j++) {
+				printf("%d\n", i);
+			}
 		}
 	}
-}
