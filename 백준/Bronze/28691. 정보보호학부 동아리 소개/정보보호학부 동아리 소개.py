5e12414694b7c7d6396543aea6db6f6dbfa95289
Teriri <108971280+Nuloi@users.noreply.github.com>
2023-08-25 03:03:56 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+char=str(input())
+
+if char=='M':
+    print('MatKor')
+elif char=='W':
+    print('WiCys')
+elif char=='C':
+    print('CyKor')
+elif char=='A':
+    print('AlKor')
+elif char=='$':
+    print('$clear')
