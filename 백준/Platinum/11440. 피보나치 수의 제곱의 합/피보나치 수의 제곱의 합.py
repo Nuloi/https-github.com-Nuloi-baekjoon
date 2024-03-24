82cacb661d85ac710e250dec40beb0993cf6f4e8
Teriri <ekgus8056@gmail.com>
2024-03-24 12:43:43 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.py
@@ -0,0 +1,32 @@
+def mat_mul(R, C) :
+    result = [[0,0], [0,0]]
+    for i in range(2):
+        for j in range(2):
+            temp = 0
+            for k in range(2):
+                temp += R[i][k] * C[k][j]
+                temp %= 1000000007
+            result[i][j] = temp
+    return result
+
+def mat_pow(matrix, n) :
+    if n == 1:
+        return matrix
+    if n % 2 == 0:
+        half = mat_pow(matrix, n // 2) 
+        return mat_mul(half, half)
+    else:
+        half = mat_pow(matrix, n // 2)
+        return mat_mul(mat_mul(half, half), matrix)
+        
+        
+n = int(input())
+
+if n == 0:
+    print("0")
+    exit(0)
+
+matrix = [[1, 1], [1, 0]]
+answer = mat_pow(matrix, n)
+print(answer[0][0]*answer[0][1] % 1000000007)
+
